c9eef55a6804264e7d7f05015e6c0498f567789a
d0kt0r_k!ddi3 <42050635+kamalakannan143@users.noreply.github.com>
2021-02-20 06:03:55 +0000

    Update colab-ssh-jupyter.sh

--- colab-ssh-jupyter.sh
+++ colab-ssh-jupyter.sh
@@ -6,7 +6,7 @@ read -p 'Enter the authtoken from ngrok :' authtoken
 ./ngrok tcp 22 &
 apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server > /dev/null
 mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc && echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 /usr/sbin/sshd -D &
 mkdir /root/.ssh
